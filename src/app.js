a501a158a2f747af98842587a69d315d32d0ace0
DESKTOP-J1TK5PG\Casey <jmlsmn@gmail.com>
2021-02-16 15:07:14 -0800

    Initial Project Setup

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(process.env.PORT, () =>
+  console.log(`App listening on port ${process.env.PORT}!`),
+);
