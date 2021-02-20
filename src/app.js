0fb110aed408938933992c70a177b7be4e6d59ba
DESKTOP-J1TK5PG\Casey <jmlsmn@gmail.com>
2021-02-19 19:02:47 -0800

    Adding html page to render results and updating readme

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
+import path from 'path';
 import routes from './routes';
 import cacheFactory from './util/cacheFactory';
 
@@ -13,7 +14,20 @@ app.use(express.urlencoded({ extended: true }));
 
 cacheFactory.init();
 
+/**
+ * REST API
+ */
 app.use('/filings', routes.filings);
+
+/**
+ * Static Files
+ */
+app.use('/', express.static('src'));
+
+// Default every route except the above to serve the index.html
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/view/index.html'));
+});
 
 app.listen(process.env.PORT, () =>
   console.log(`App listening on port ${process.env.PORT}!`),
